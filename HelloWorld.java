e11d0a60f4f8f2c5589ef31be546c5226db37c0c
gpandi9566 <111579199+gpandi9566@users.noreply.github.com>
2022-08-19 06:21:50 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println(" Welcome to Java..... ");
+		System.out.println(" Welcome to Java Gokul..... ");
 
 	}
 
